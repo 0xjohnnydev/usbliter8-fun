086ca09ba6656da897daed1a70a7bfae32d6bdfa
Johnny Franks <johnnycfranks@gmail.com>
2026-09-12 21:58:48 -0400

    Fail closed before restore bootstrap

--- work-27.0-rc-iphone11/restore_cfw.sh
+++ work-27.0-rc-iphone11/restore_cfw.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
-../tools/usbliter8ctl boot ./Ramdisk/iBSS.raw;
-sleep 3;
+set -e
 
-idevicerestore -s "http://127.0.0.1:1337" -e -y CFW;
+PYUSB_PYTHON=${USBLITER8_PYTHON:-python3}
+if ! "$PYUSB_PYTHON" -c 'import usb' >/dev/null 2>&1; then
+    print -u2 "Python '$PYUSB_PYTHON' does not have PyUSB; refusing to touch the device"
+    exit 1
+fi
+
+"$PYUSB_PYTHON" ../tools/usbliter8ctl boot ./Ramdisk/iBSS.raw
+sleep 3
+
+idevicerestore -s "http://127.0.0.1:1337" -e -y CFW
